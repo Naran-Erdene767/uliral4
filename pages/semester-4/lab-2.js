9b67434bfb59d1e8bd20dbd832d8282882799152
Naran-Erdene <narkasuldee@gmail.com>
2025-05-02 00:28:42 +0800

    asd

--- pages/semester-4/lab-2.js
+++ pages/semester-4/lab-2.js
@@ -1,36 +1,46 @@
-import { useState, useEffect } from "react";
+    import { Inter } from 'next/font/google'
+import { useState, useEffect } from 'react';
 
-export default function Lab2() {
-    const [clothes, setClothes] = useState([]);
-    const [error, setError] = useState(null);
+const inter = Inter({ subsets: ['latin'] })
+
+export default function Home() {
+
+    const [data, setData] = useState("");
     const [loading, setLoading] = useState(true);
 
     useEffect(() => {
         async function fetchData() {
             try {
-                const response = await fetch('https://mongol-api-rest.vercel.app/clothes');
-                const data = await response.json();
-                console.log(data);
-                setClothes(data);
-            } catch (error) {
-                console.error(error);
-                setError(error);
-            } finally {
+                setLoading(true);
+                const response = await fetch(
+                    "https://mongol-api-rest.vercel.app/clothes"
+                );
+                const result = await response.json();
+                setData(result);
                 setLoading(false);
+                console.log(result);
+            } catch (error) {
+                console.error( error);
             }
         }
-
         fetchData();
     }, []);
 
     return (
-        <div>
-            <h1>Clothes List</h1>
-                <ul>
-                    {clothes.map((item) => (
-                        <li key={item.id || item.name}>{item.name}</li>
-                    ))}
-                </ul>
+        <div className='h-screen w-screen p-20'>
+            <div className="h-full w-full rounded-xl p-4 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4">
+                {loading ? (
+                    <p className={`${inter.className} text-xl text-gray-400`}>Loading...</p>
+                ) : (
+                    data.clothes.map((item) => (
+                        <div key={item.id} className="bg-purple-800 p-4 rounded-lg w-full h-96 flex flex-col justify-center items-center">
+                            <img src={item.images[0]} alt={item.name} width={120} height={120} className="rounded-lg" />
+                            <h2 className={`${inter.className} text-2xl text-white mt-2`}>{item.name}</h2>
+                            <p className={`${inter.className} text-white mt-2`}>{item.description}</p>
+                        </div>
+                    ))
+                )}
+            </div>
         </div>
-    );
+    )
 }
